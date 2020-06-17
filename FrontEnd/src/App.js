374fcf6d539360eafe5391314576d1ec0622b12d
Amina <aminalaabidi2018@gmail.com>
2020-06-17 16:35:39 +0100

    first commit

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="ui button">Click Here</button>
+      <button class="ui button">Show Modal</button>
+    </div>
+  );
+}
+
+export default App;
